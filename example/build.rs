bb9d5a3cb47e26f94cfbe802f7d0092950e4c1d9
Thomas Flaherty <thomas.flaherty@dyson.com>
2026-05-26 10:46:53 +0100

    half updated example to use new compressor trait

--- example/build.rs
+++ example/build.rs
@@ -1,11 +1,26 @@
-use asset_compression::{TargetColorFormat, rebuild_graphics_if_changed};
+use asset_compression::{Compressor, TargetColorFormat, rebuild_graphics_if_changed};
+
+struct ZlibCompressor {}
+
+impl Compressor for ZlibCompressor {
+    fn compress(&self, input_bytes: &[u8]) -> Result<Vec<u8>, ()> {
+        const COMPRESSION_LEVEL: u8 = 5;
+        Ok(miniz_oxide::deflate::compress_to_vec(
+            input_bytes,
+            COMPRESSION_LEVEL,
+        ))
+    }
+}
 
 fn main() {
     // todo: Does the position of this function within main affect output?
+    let zlib_compressos = ZlibCompressor {};
+
     rebuild_graphics_if_changed(
         "./graphics-src",
         "./graphics-bin",
         TargetColorFormat::Rgb565,
+        zlib_compressos,
     )
     .unwrap();
 
